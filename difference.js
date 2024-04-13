7620272e4ff544874d3eb33a09f2dbb79a633fd9
Nishapandit1 <nishapandit135135@gmail.com>
2024-04-13 07:27:33 +0530

    updated code

--- difference.js
+++ difference.js
@@ -1,22 +1,22 @@
-'use strict';
-
 const puppeteer = require('puppeteer');
 const fs = require('fs');
 const PNG = require('pngjs').PNG;
 const pixelmatch = require('pixelmatch');
-const pages =require('./pages.json');
+const pages =require('./config/pages.json');
+
 (async () => {
   const browser = await puppeteer.launch();
   try {
     const page = await browser.newPage();
-    for (const { pageName,stagingUrl } of pages)  {
-        await page.goto(stagingUrl);
-        //  await page.setViewport({ width: 1440, height: 1080 });
-        await page.screenshot({ path: `Screenshorts/production/${pageName}.png`,fullPage:true,}); 
-        
+    for (const { pageName,staging,production} of pages['urls'])  {
+        await page.goto(staging);
+        await page.screenshot({ path: `Screenshorts/staging/${pageName}.png`,fullPage:true}); 
 
-    const img1 = PNG.sync.read(fs.readFileSync(`Screenshorts/production/${pageName}.jpeg`));
-    const img2 = PNG.sync.read(fs.readFileSync(`Screenshorts/production/st.png`));
+        await page.goto(production);
+        await page.screenshot({ path: `Screenshorts/production/${pageName}.png`,fullPage:true}); 
+    
+    const img1 = PNG.sync.read(fs.readFileSync(`Screenshorts/staging/${pageName}.png`));
+    const img2 = PNG.sync.read(fs.readFileSync(`Screenshorts/production/${pageName}.png`));
 
     const { width, height } = img1;
     const diff = new PNG({ width, height });
@@ -24,7 +24,8 @@ const pages =require('./pages.json');
     const result = pixelmatch(img1.data, img2.data, diff.data, width, height, { threshold: 0.1 }); 
       fs.writeFileSync(`Screenshorts/difference/${pageName}.jpeg`, PNG.sync.write(diff));
 }
-  } catch (e) {
+  } 
+catch (e) {
     console.log(e)
   } finally {
     await browser.close();
